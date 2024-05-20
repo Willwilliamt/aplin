7ac7249df9a7c893ff2aff20bbb824552fabd0fa
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-05-20 23:18:57 +0700

    #8 ui security admin

--- topupgame/app/Http/Controllers/AuthController.php
+++ topupgame/app/Http/Controllers/AuthController.php
@@ -9,34 +9,25 @@ use Illuminate\Support\Facades\Session;
 class AuthController extends Controller
 {
     public function login(Request $request) {
-        $request->validate([
-            'username'=>'required',
-            'password'=>'required'
-        ],[
-            'username.required'=>'wajib diisi',
-            'password.required'=>'wajib diisi'
-        ]);
         $username = $request->input('username');
         $password = $request->input('password');
 
         $pengguna = Pengguna::where('username', $username)
                              ->where('password', $password)
                              ->first();
-
         if($pengguna) {
             $request->session()->put('user',$username);
             $request->session()->put('user_id', $pengguna->Id_user);
-            if ($pengguna->role == 2) {
+            if ($pengguna->Role == 2) {
                return redirect('/superadmin');
            }
-           elseif ($pengguna->role == 1) {
+           else if ($pengguna->Role == 1) {
                return redirect('/securityadmin');
            }
-            else if ($pengguna->role == 0) {
+            else if ($pengguna->Role == 0) {
                 return redirect('/');
             } 
         } else {
-
             return back()->with('error', 'Username atau password salah.');
         }
     }
