bbbe0e6e25ffcea2d6a7b7fbac14cad58deef982
AhoDanshi <martinsudjaya@gmail.com>
2024-05-21 17:22:37 +0700

    #11 benerin merge yang nabrak
    sebelumnya lupa merge ke main jadi rico lupa kalo controller harusnya nabrak sama punyaku

--- topupgame/routes/web.php
+++ topupgame/routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\PenggunaController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +17,7 @@ use App\Http\Controllers\KategoriController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [KategoriController::class, 'home'])->name('home');
 Route::get('/login', function () {
     return view('login');
 });
@@ -43,33 +40,29 @@ Route::prefix('superadmin')->group(function () {
     Route::post('/demote', [PenggunaController::class, 'demote']);
 });
 
-
 Route::prefix('pengguna')->group(function () {
     Route::post('/insert', [PenggunaController::class, 'insert']);
     Route::post('/update', [PenggunaController::class, 'update']);
     Route::post('/delete', [PenggunaController::class, 'delete']);
     Route::post('/login', [AuthController::class, 'login']);
-    });
+});
 
 Route::prefix('user')->group(function () {
     Route::post('/insert', [BarangController::class, 'insert']);
 });
+
 Route::prefix('kategori')->group(function () {
     Route::post('/insert', [KategoriController::class, 'insert']);
+    Route::get('show/{id}', [KategoriController::class, 'show'])->name('kategori.show');
+    Route::put('edit/{id}', [KategoriController::class, 'updateCategory'])->name('kategori.update');
+    Route::delete('destroy/{id}', [KategoriController::class, 'destroyCategory'])->name('kategori.destroy');
 });
+
 Route::controller(BarangController::class)->prefix('products')->group(function () {
     Route::get('show/{id}', 'show')->name('products.show');  
     Route::put('edit/{id}', 'update')->name('products.update');
     Route::delete('destroy/{id}', 'destroy')->name('products.destroy');
 });
 
-Route::controller(KategoriController::class)->prefix('kategori')->group(function () {
-    Route::get('show/{id}', 'show')->name('kategori.show');  
-    Route::put('edit/{id}', 'update')->name('kategori.update');
-    Route::delete('destroy/{id}', 'destroy')->name('kategori.destroy');
-});
-
-
 Route::get('/logout', [PenggunaController::class, 'logout'])->name('logout');
 Route::get('/signup', [PenggunaController::class, 'signup'])->name('signup');
-Route::get('/', [KategoriController::class, 'home'])->name('home');
