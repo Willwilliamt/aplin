d46a536db37feb9fc88f8f2d2085104c842fad53
Kyora02 <kedrick.adyatma@gmail.com>
2024-05-22 13:42:27 +0700

    UI Design cruduser udah

--- topupgame/resources/views/cruduser.blade.php
+++ topupgame/resources/views/cruduser.blade.php
@@ -10,58 +10,77 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         body{
-            
+            background: linear-gradient(#F1EAFF,#E5D4FF,#DCBFFF);
+            height: 100vh; 
+            margin: 0;
+        }
+        .box{
+            background: linear-gradient(#F1EAFF,#E5D4FF,#DCBFFF);
+            padding: 2rem;
+            border-radius: 10px;
+            border: 1px solid black;
+        }
+        .text{
+            color: #F1EAFF;
+        }
+        .bodyTable{
+            background-color: #D0A2F7;
+        }
+        .buton{
+            background: #E7BCDE;
+        }
+        .buton:hover {
+            background: rgba(255, 255, 255, 0.3);
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <span class="navbar-text text-white me-2">
+    <div class="d-flex justify-content-center" style="height: 100vh; align-items: flex-start; padding-top: 10vh;">
+        <span class="navbar-text text me-2">
             Welcome, {{ Session::get('user') }}!
         </span>
-        <div class="d-flex align-items-center justify-content-between">
-            <h1 class="mb-0">List Barang</h1>
-            <a href="/" class="btn btn-primary">BACK</a>
-            <a href="/addbarang" class="btn btn-primary">Add Barang</a>
-        </div>
-        <hr />
-        <table class="table table-hover" >
-            <thead class="table-primary">
-                <tr>
-                    
-                    <th>ID</th>
-                    <th>Nama Barang</th>
-                    <th>Harga Barang</th>
-                    <th>Kategori</th>
-                    <th>Foto Barang</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($barangs as $barang)
-                    <tr>
-                        
-                        <td class="align-middle">{{ $barang->Id_barang }}</td>
-                        <td class="align-middle">{{ $barang->Nama_barang }}</td>
-                        <td class="align-middle">{{ $barang->Harga_barang }}</td>
-                        <td class="align-middle">{{ $barang->kategori->nama_kategori}}</td>
-                        <td class="align-middle"><img src="{{ asset('uploads/barang/' . $barang->image) }}" alt="" width="100px" height="100px"></td>
-                        <td>
-                            <div class="btn-group" role="group" aria-label="Basic Example">
-                                <a href="{{ route('products.show', $barang->Id_barang) }}" type="button" class="btn btn-secondary">UPDATE</a>
-                                <form action="{{ route('products.destroy', $barang->Id_barang) }}" method="POST" onsubmit="return confirm('Delete?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger m-0">Delete</button>
-                                </form>
-                                
-                            </div>
-                        </td>
+        <div class="container box p-5">
+            <a href="/" class="btn buton">Back</a>
+            <div class="">
+                <h1 class="text-center">List Barang</h1>
+                <a href="/addbarang" class="btn buton">Add Barang</a>
+            </div>
+            <hr />
+            <table class="table table-hover" >
+                <thead class="table-primary text-center">
+                    <tr>         
+                        <th>ID</th>
+                        <th>Nama Barang</th>
+                        <th>Harga Barang</th>
+                        <th>Kategori</th>
+                        <th>Foto Barang</th>
+                        <th>Action</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-
+                </thead>
+                <tbody class="bodyTable text-center">
+                    @foreach ($barangs as $barang)
+                        <tr>                
+                            <td class="align-middle">{{ $barang->Id_barang }}</td>
+                            <td class="align-middle">{{ $barang->Nama_barang }}</td>
+                            <td class="align-middle">{{ $barang->Harga_barang }}</td>
+                            <td class="align-middle">{{ $barang->kategori->nama_kategori}}</td>
+                            <td class="align-middle"><img src="{{ asset('uploads/barang/' . $barang->image) }}" alt="" width="100px" height="100px"></td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Basic Example">
+                                    <a href="{{ route('products.show', $barang->Id_barang) }}" type="button" class="btn btn-secondary">UPDATE</a>
+                                    <form action="{{ route('products.destroy', $barang->Id_barang) }}" method="POST" onsubmit="return confirm('Delete?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger m-0">Delete</button>
+                                    </form>
+                                    
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>  
+        </div>
     </div>
 
 </body>
