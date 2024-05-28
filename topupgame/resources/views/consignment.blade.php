fb2b9bfb3766cccc1d9675876075533e13c890ae
ricoricko <ricofrede20@gmail.com>
2024-05-28 22:39:29 +0700

    #2 Mengubah agar menggunakan yiel content section
    #2 Mengubah agar menggunakan yiel content section

--- topupgame/resources/views/consignment.blade.php
+++ topupgame/resources/views/consignment.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-TlZzthgzo+Oj0bgdJ2yRbYNzeIW2alA0wulb1LV2a4oYYMMsmWi+0MaZ2MtbAs+leEKRkYOyBjZfHKR7FVT8wg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-    <div class="container">
-        <h2>Consignment</h2>
-        <h3>Welcome, {{ Session::get('user') }}!</h3><br>
-        <a href="/" class="btn btn-secondary">Back</a>
+@extends('template.hometemplate')
+@section('consign')
+    <br>
+<h1 class="text-center">Consign</h1>
         <div style="display: flex; justify-content: space-evenly;">
             @foreach ($barang as $index)
             <div class="card" style="width: 18rem;">
@@ -30,7 +19,6 @@
                 </div>
             </div>
             @endforeach
-        </div>
-    </div>
-</body>
-</html>
+        </div><br>
+
+@endsection
