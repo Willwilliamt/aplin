3fe1cd832af5906895fa3984f31a67ba08be5228
ricoricko <ricofrede20@gmail.com>
2024-06-06 01:06:40 +0700

    #24 Show
    #24 Admin dapat lihat transaksi consign dan influencer

--- topupgame/app/Http/Controllers/TransaksiController.php
+++ topupgame/app/Http/Controllers/TransaksiController.php
@@ -4,28 +4,49 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\TransaksiTopUpGame;
+use App\Models\transaksiInfluencer;
+use App\Models\transaksiConsign;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class TransaksiController extends Controller
 {
     public function order(Request $request)
     {
-        // Validasi input
+        
         $validatedData = $request->validate([
             'user_id' => 'required|exists:users,Id_user',
             'game_name' => 'required|string',
             'jumlah_topup' => 'required|integer',
         ]);
 
-        // Buat entri transaksi top-up game
+        
         $transaksi = new TransaksiTopUpGame();
         $transaksi->id_user = $validatedData['user_id'];
         $transaksi->game_name = $validatedData['game_name'];
         $transaksi->jumlah_topup = $validatedData['jumlah_topup'];
-        $transaksi->Tanggal_transaksi = now(); // Menggunakan tanggal dan waktu saat ini
+        $transaksi->Tanggal_transaksi = now(); 
 
-        // Simpan transaksi ke database
         $transaksi->save();
 
         return response()->json(['message' => 'Transaksi berhasil'], 200);
+    }
+
+    public function viewAdmin(Request $request)
+    {
+        $trans = DB::table('transaksiConsign')
+        ->join('Barang', 'transaksiConsign.id_barang', '=', 'Barang.Id_barang')
+        ->join('users as pembeli', 'transaksiConsign.id_user', '=', 'pembeli.Id_user')
+        ->join('users as penjual', 'transaksiConsign.id_seller', '=', 'penjual.Id_user')
+        ->select('transaksiConsign.*', 'Barang.Nama_barang', 'pembeli.name as pembeli', 'penjual.name as penjual')
+        ->get();
+
+        $trans2 = DB::table('transaksi_influencer')
+        ->join('Influencer', 'transaksi_influencer.id_influencer', '=', 'Influencer.Id_influencer')
+        ->select('transaksi_influencer.*', 'Influencer.Nama_influencer')
+        ->get();
+
+
+        return view('viewadmin', compact('trans'), compact('trans2'));
     }
 }
