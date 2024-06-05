c593e4251e1136fd962101b7ba0fc43fb56b8ac5
AhoDanshi <martinsudjaya@gmail.com>
2024-06-05 09:52:25 +0700

    #21

--- topupgame/app/Http/Controllers/PaymentController.php
+++ topupgame/app/Http/Controllers/PaymentController.php
@@ -10,17 +10,14 @@ class PaymentController extends Controller
 {
     public function getSnapToken(Request $request)
 {
-    // Set your Merchant Server Key
     Config::$serverKey = 'SB-Mid-server-84hdmckw7ImB5-ZLX0YS-1aj';
-    // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
     Config::$isProduction = false;
-    // Set sanitization on (default)
+  
     Config::$isSanitized = true;
-    // Set 3DS transaction for credit card to true
     Config::$is3ds = true;
 
     $orderId = uniqid();
-    $grossAmount = $request->product_price * $request->quantity; // Calculate the total amount based on the product price and quantity
+    $grossAmount = $request->product_price * $request->quantity; 
 
     $transactionDetails = [
         'order_id' => $orderId,
@@ -32,12 +29,12 @@ class PaymentController extends Controller
             'id' => $request->product_id,
             'price' => $request->product_price,
             'quantity' => $request->quantity,
-            'name' => 'Product Name', // Example: get product name from DB
+            'name' => 'Product Name', 
         ],
     ];
 
     $customerDetails = [
-        'first_name' => 'Customer', // Example: get customer details from request or DB
+        'first_name' => 'Customer', 
         'last_name' => 'Name',
         'email' => 'customer@example.com',
         'phone' => '081234567890',
